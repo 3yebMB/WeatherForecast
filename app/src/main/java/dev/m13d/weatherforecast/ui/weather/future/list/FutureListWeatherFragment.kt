6fa6b2bac93d70604bdb1ff49cf0fc4ec92f055f
Michael <3yebmb@gmail.com>
2019-07-19 14:49:45 +0500

    add weather future UI

--- app/src/main/java/dev/m13d/weatherforecast/ui/weather/future/list/FutureListWeatherFragment.kt
+++ app/src/main/java/dev/m13d/weatherforecast/ui/weather/future/list/FutureListWeatherFragment.kt
@@ -2,18 +2,29 @@ package dev.m13d.weatherforecast.ui.weather.future.list
 
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.ViewHolder
 import dev.m13d.weatherforecast.R
+import dev.m13d.weatherforecast.data.db.unitlocalized.future.UnitSpecificSimpleFutureWeatherEntry
+import dev.m13d.weatherforecast.ui.base.ScopedFragment
+import kotlinx.android.synthetic.main.future_list_weather_fragment.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.x.closestKodein
+import org.kodein.di.generic.instance
 
-class FutureListWeatherFragment : Fragment() {
+class FutureListWeatherFragment : ScopedFragment(), KodeinAware {
 
-    companion object {
-        fun newInstance() = FutureListWeatherFragment()
-    }
+    override val kodein by closestKodein()
+    private val viewModelFactory: FutureListWeatherViewModelFactory by instance()
 
     private lateinit var viewModel: FutureListWeatherViewModel
 
@@ -26,8 +37,55 @@ class FutureListWeatherFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(FutureListWeatherViewModel::class.java)
-        // TODO: Use the ViewModel
+        viewModel = ViewModelProviders.of(this, viewModelFactory)
+            .get(FutureListWeatherViewModel::class.java)
+        bindUI()
+    }
+
+    private fun bindUI() = launch(Dispatchers.Main) {
+        val futureWeatherEntries = viewModel.weatherEntries.await()
+        val weatherLocation = viewModel.weatherLocation.await()
+
+        weatherLocation.observe(this@FutureListWeatherFragment, Observer { location ->
+            if (location == null) return@Observer
+            updateLocation(location.name)
+        })
+
+        futureWeatherEntries.observe(this@FutureListWeatherFragment, Observer { weatherEntries ->
+            if (weatherEntries == null) return@Observer
+            group_loading.visibility = View.GONE
+            updateDateToNextWeek()
+            initRecyclerView(weatherEntries.toFutureWeatherItems())
+        })
+    }
+
+    private fun List<UnitSpecificSimpleFutureWeatherEntry>.toFutureWeatherItems() : List<FutureWeatherItem> {
+        return this.map {
+            FutureWeatherItem(it)
+        }
+    }
+
+    private fun initRecyclerView(items: List<FutureWeatherItem>) {
+        val groupAdapter = GroupAdapter<ViewHolder>().apply {
+            addAll(items)
+        }
+
+        recyclerView.apply {
+            layoutManager = LinearLayoutManager(this@FutureListWeatherFragment.context)
+            adapter = groupAdapter
+        }
+
+        groupAdapter.setOnItemClickListener { item, view ->
+            Toast.makeText(this@FutureListWeatherFragment.context, "Clicked", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun updateLocation(location: String) {
+        (activity as AppCompatActivity)?.supportActionBar?.title = location
+    }
+
+    private fun updateDateToNextWeek() {
+        (activity as AppCompatActivity)?.supportActionBar?.subtitle = "Next Week"
     }
 
 }
---
+++ app/src/main/java/dev/m13d/weatherforecast/ui/weather/future/list/FutureWeatherItem.kt
@@ -0,0 +1,44 @@
+package dev.m13d.weatherforecast.ui.weather.future.list
+
+import android.annotation.SuppressLint
+import com.xwray.groupie.kotlinandroidextensions.Item
+import com.xwray.groupie.kotlinandroidextensions.ViewHolder
+import dev.m13d.weatherforecast.R
+import dev.m13d.weatherforecast.data.db.unitlocalized.future.MetricSimpleFutureWeatherEntry
+import dev.m13d.weatherforecast.data.db.unitlocalized.future.UnitSpecificSimpleFutureWeatherEntry
+import dev.m13d.weatherforecast.internal.glide.GlideApp
+import kotlinx.android.synthetic.main.item_future_weather.*
+import org.threeten.bp.format.DateTimeFormatter
+import org.threeten.bp.format.FormatStyle
+
+class FutureWeatherItem(
+    val weatherEnty: UnitSpecificSimpleFutureWeatherEntry
+) : Item(){
+    override fun bind(viewHolder: ViewHolder, position: Int) {
+        viewHolder.apply {
+            textView_condition.text = weatherEnty.conditionText
+            updateDate()
+            updateTemperature()
+            updateConditionImage()
+        }
+    }
+
+    override fun getLayout() = R.layout.item_future_weather
+
+    private fun ViewHolder.updateDate() {
+        val dtFormatter = DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM)
+        textView_date.text = weatherEnty.date.format(dtFormatter)
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun ViewHolder.updateTemperature() {
+        val unitAbbreviation = if (weatherEnty is MetricSimpleFutureWeatherEntry) "°C" else "°F"
+        textView_temperature.text = "${weatherEnty.avgTemperature}$unitAbbreviation"
+    }
+
+    private fun ViewHolder.updateConditionImage() {
+        GlideApp.with(this.containerView)
+            .load("https:"+weatherEnty.conditionIconUrl)
+            .into(imageView_condition_icon)
+    }
+}
